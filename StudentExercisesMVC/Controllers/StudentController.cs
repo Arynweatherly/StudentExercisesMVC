64c9be68de36f3f30634527395aaa347bb9a7570
aryn weatherly <arynweatherly93@gmail.com>
2020-01-14 14:40:39 -0600

    first commit

---
+++ StudentExercisesMVC/Controllers/StudentController.cs
@@ -0,0 +1,174 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using StudentExercisesMVC.Models;
+
+namespace StudentExercisesMVC.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly IConfiguration _config;
+        public StudentsController(IConfiguration config)
+        {
+            _config = config;
+        }
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+        // GET: Students
+        public ActionResult Index()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+            SELECT s.Id,
+                s.FirstName,
+                s.LastName,
+                s.SlackHandle,
+                s.CohortId
+            FROM Student s
+        ";
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    List<Student> students = new List<Student>();
+                    while (reader.Read())
+                    {
+                        Student student = new Student
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                            LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                            SlackHandle = reader.GetString(reader.GetOrdinal("SlackHandle")),
+                            CohortId = reader.GetInt32(reader.GetOrdinal("CohortId"))
+                        };
+                        students.Add(student);
+                    }
+                    reader.Close();
+                    return View(students);
+                }
+            }
+        }
+        // GET: Students/Details/5
+        public ActionResult Details(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT c.Id as CohortId, c.Name, s.Id as StudentId, s.FirstName, s.LastName, s.SlackHandle, s.CohortId
+FROM Student s
+LEFT JOIN Cohort c ON c.Id = s.CohortId
+WHERE s.Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+                    var reader = cmd.ExecuteReader();
+
+                    Student student = null;
+
+
+                    while (reader.Read())
+                    {
+                        if (student == null)
+                        {
+                            student = new Student
+
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("StudentId")),
+                                FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                                LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                                SlackHandle = reader.GetString(reader.GetOrdinal("SlackHandle")),
+                                CohortId = reader.GetInt32(reader.GetOrdinal("CohortId")),
+                                Cohort = new Cohort()
+                        };
+                        }
+                        var hasCohort = !reader.IsDBNull(reader.GetOrdinal("CohortId"));
+                        if (hasCohort)
+                        {
+                            student.Cohort = new Cohort()
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("CohortId")),
+                                Name = reader.GetString(reader.GetOrdinal("Name")),
+                            };
+                        }
+                    }
+
+                        reader.Close();
+                    if (student == null)
+                    {
+                        return NotFound();
+                    }
+                        return View(student);
+       
+                }
+            }
+        }
+        // GET: Students/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+        // POST: Students/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        // GET: Students/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+        // POST: Students/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        // GET: Students/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+        // POST: Students/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
